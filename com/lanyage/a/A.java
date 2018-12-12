b88b3b00451aca87ef2a80ee438220fe75f434e3
lllyyyggg <962221804@qq.com>
2018-12-13 00:24:31 +0800

    initial commit

---
+++ com/lanyage/a/A.java
@@ -0,0 +1,6 @@
+package com.lanyage.a;
+public class A {
+	public String getName() {
+		return "This is a";
+	}
+}
---
+++ com/lanyage/b/B.java
@@ -0,0 +1,8 @@
+package com.lanyage.b;
+import com.lanyage.a.A;
+public class B {
+	public static void main(String[] args) {
+		A a = new A();
+		System.out.println(a.getName());
+	}
+}
